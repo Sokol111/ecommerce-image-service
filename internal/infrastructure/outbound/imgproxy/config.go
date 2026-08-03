6d3908bdb2aa54e9ff7e7c3413fb8d29cb1e5014
Sokol111 <igorsokol111@gmail.com>
2026-08-03 15:55:10 +0200

    u

--- internal/infrastructure/outbound/imgproxy/config.go
+++ internal/infrastructure/outbound/imgproxy/config.go
@@ -6,18 +6,16 @@ import (
 )
 
 type Config struct {
-	PublicBaseURL  string `koanf:"public-base-url"`
-	Bucket         string `koanf:"bucket"`
-	KeyHex         string `koanf:"key-hex"`
-	SaltHex        string `koanf:"salt-hex"`
-	DefaultQuality int
-	Key            []byte
-	Salt           []byte
+	PublicBaseURL string `koanf:"public-base-url"`
+	Bucket        string `koanf:"bucket"`
+	KeyHex        string `koanf:"key-hex"`
+	SaltHex       string `koanf:"salt-hex"`
+	Key           []byte
+	Salt          []byte
 }
 
 func (c *Config) ApplyDefaults() {
 	c.PublicBaseURL = strings.TrimRight(c.PublicBaseURL, "/")
-	c.DefaultQuality = 80
 }
 
 func (c *Config) Validate() error {
@@ -29,9 +27,6 @@ func (c *Config) Validate() error {
 	}
 	if len(c.Salt) == 0 {
 		return errors.New("imgproxy salt is required")
-	}
-	if c.DefaultQuality < 1 || c.DefaultQuality > 100 {
-		return errors.New("imgproxy default quality must be between 1 and 100")
 	}
 	if c.Bucket == "" {
 		return errors.New("imgproxy bucket is required")
